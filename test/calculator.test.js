0c86e1dda838696d137901721c021cdec5cf7cb9
Guilherme Alves <guiigos.alves@gmail.com>
2026-03-24 11:34:53 -0300

    Pattern scanning

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,5 @@
-const it = require('tape');
-const calculator = require('../src/calculator');
+const it = require("tape");
+const calculator = require("../src/calculator");
 
 /*
 ███████╗███╗   ███╗ ██████╗ ██╗  ██╗███████╗    ████████╗███████╗███████╗████████╗███████╗
@@ -10,34 +10,34 @@ const calculator = require('../src/calculator');
 ╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝       ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚══════╝
 */
 
-it('should exist the calculator lib', (test) => {
+it("should exist the calculator lib", (test) => {
   test.plan(1);
   test.notEqual(calculator, undefined);
 });
 
-it('should exist the method `sum`', (test) => {
+it("should exist the method `sum`", (test) => {
   test.plan(2);
   test.notEqual(calculator.sum, undefined);
-  test.equal(typeof calculator.sum, 'function');
+  test.equal(typeof calculator.sum, "function");
 });
 
-it('should exist the method `sub`', (test) => {
+it("should exist the method `sub`", (test) => {
   test.plan(2);
   test.notEqual(calculator.sub, undefined);
-  test.equal(typeof calculator.sub, 'function');
+  test.equal(typeof calculator.sub, "function");
 });
 
-it('should exist the method `div`', (test) => {
+it("should exist the method `div`", (test) => {
   test.plan(2);
   test.notEqual(calculator.div, undefined);
-  test.equal(typeof calculator.div, 'function');
+  test.equal(typeof calculator.div, "function");
 });
 
 
-it('should exist the method `mult`', (test) => {
+it("should exist the method `mult`", (test) => {
   test.plan(2);
   test.notEqual(calculator.mult, undefined);
-  test.equal(typeof calculator.mult, 'function');
+  test.equal(typeof calculator.mult, "function");
 });
 
 /*
@@ -49,37 +49,37 @@ it('should exist the method `mult`', (test) => {
  ╚═════╝ ╚═╝  ╚═══╝╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝          ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚══════╝
 */
 
-it('should return 4 when `sum(2, 2)`', (test) => {
+it("should return 4 when `sum(2, 2)`", (test) => {
   test.plan(1);
   test.equal(calculator.sum(2, 2), 4);
 });
 
-it('should return 4 when `sub(6, 2)`', (test) => {
+it("should return 4 when `sub(6, 2)`", (test) => {
   test.plan(1);
   test.equal(calculator.sub(6, 2), 4);
 });
 
-it('should return -4 when `sub(6, 10)`', (test) => {
+it("should return -4 when `sub(6, 10)`", (test) => {
   test.plan(1);
   test.equal(calculator.sub(6, 10), -4);
 });
 
-it('should return 4 when `mult(2, 2)`', (test) => {
+it("should return 4 when `mult(2, 2)`", (test) => {
   test.plan(1);
   test.equal(calculator.mult(2, 2), 4);
 });
 
-it('should return 1 when `mult(1, 1)`', (test) => {
+it("should return 1 when `mult(1, 1)`", (test) => {
   test.plan(1);
   test.equal(calculator.mult(1, 1), 1);
 });
 
-it('should return 2 when `div(4, 2)`', (test) => {
+it("should return 2 when `div(4, 2)`", (test) => {
   test.plan(1);
   test.equal(calculator.div(4, 2), 2);
 });
 
-it('should return `Infinity` when divide by 0', (test) => {
+it("should return `Infinity` when divide by 0", (test) => {
   test.plan(1);
   test.equal(calculator.div(2, 0), Infinity);
 });
